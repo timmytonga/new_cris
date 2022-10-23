c609af8c3f992b0f00f1b50d6631540e796e7c96
thien <nguyen.thien@northeastern.edu>
2022-10-23 14:32:41 -0400

    fixed civilcomments

--- scripts/wandb_sweeps/civilComments_sweep.py
+++ scripts/wandb_sweeps/civilComments_sweep.py
@@ -11,15 +11,16 @@ args = get_args()
 sweep_configuration = get_default_sweep_config(dataset_name, PROJECT_NAME, args)
 sweep_configuration['parameters'] = {
     'jigsaw_use_group':
-        {'values': ['black', 'any_identity']},
+        {'values': ['any_identity']},
     'part2_batch_size':
-        {'values': [24, 16, 32]},
+        {'values': [24, 16]},
     'part2_lr':
         {'values': ['1e-2', '1e-3', '1e-4', '1e-5']},
     'part2_wd':
-        {'values': ['1e-1', '1e-2', '1e-3', 0]},
+        {'values': ['1e-1', '1e-4', 0]},
     'reduce_val_fraction':
         {'value': 0.05}
 }
+sweep_configuration['name'] = f'Seed {args.seed} p={args.p} new fixed val'
 
 sweep_id = wandb.sweep(sweep=sweep_configuration, project=f'{PROJECT_NAME}_{dataset_name}')
